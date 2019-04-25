5938279e5e543882924f14545b3b877c8ebc8c35
Renan Castro <renanccastro@gmail.com>
2019-04-25 15:43:46 -0300

    corrigindo import errado do snackbar

--- src/ui/SnackbarWithProps.js
+++ src/ui/SnackbarWithProps.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Snackbar } from '@tecsinapse/ui-kit';
+import { Snackbar } from '@tecsinapse/ui-kit/build/Notifications/Snackbar';
 
 export const SnackbarWithProps = ({ show, dismiss, variant, text }) => (
   <Snackbar show={show} variant={variant} onClose={dismiss}>
